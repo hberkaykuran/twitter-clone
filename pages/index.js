60232538034949858dae5dafb65ef9ad7de0340c
hberkaykuran <berkaykuran54@gmail.com>
2022-10-31 18:21:23 +0300

    init

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Twitter Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Home Page
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+        {/* Modal */}
+      </main>
+    </div>
+  );
+}
