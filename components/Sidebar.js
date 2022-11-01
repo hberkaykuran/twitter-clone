39ff570157e6062670bf5b3e746275f4e60af9f5
hberkaykuran <berkaykuran54@gmail.com>
2022-11-02 02:07:26 +0300

    Sidebar improvements

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -8,17 +8,17 @@ import {
     BookmarkIcon,
     ClipboardIcon,
     UserIcon,
-    DotsCircleHorizontalIcon,
+    EllipsisHorizontalCircleIcon,
     DotsHorizontalIcon,
 } from "@heroicons/react/24/outline";
 
 function Sidebar() {
     return (
-        <div className=" sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
+        <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
             <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
                 <Image src="https://rb.gy/ogau5a" width={30} height={30} />
             </div>
-            <div className="space-y-2.5">
+            <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-245">
                 <SidebarLink text="Home" Icon={HomeIcon} active />
                 <SidebarLink text="Explore" Icon={HashtagIcon} />
                 <SidebarLink text="Notifications" Icon={BellIcon} />
@@ -26,7 +26,7 @@ function Sidebar() {
                 <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
                 <SidebarLink text="Lists" Icon={ClipboardIcon} />
                 <SidebarLink text="Profile" Icon={UserIcon} />
-                <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
+                <SidebarLink text="More" Icon={EllipsisHorizontalCircleIcon} />
             </div>
         </div>
     )
