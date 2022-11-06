673d3a14c2f9baa0ba5314e30f6edd29a802bd80
hberkaykuran <berkaykuran54@gmail.com>
2022-11-06 10:58:01 +0300

    Post component draft, added moment and recoil

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,24 @@
 import { SparklesIcon } from "@heroicons/react/24/outline";
 import Input from "./Input";
+import Post from "./Post";
+import { useEffect, useState } from "react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    [db]
+  );
+
   return (
     <div className="text-[#d9d9d9] flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
       <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700">
@@ -10,8 +27,12 @@ export default function Feed() {
           <SparklesIcon className="h-5 text-[#d9d9d9]" />
         </div>
       </div>
-      {/* Fetch data */}
       <Input />
+      <div className="pb-72">
+        {posts.map((post) => (
+          <Post key={post.id} id={post.id} post={post.data()} />
+        ))}
+      </div>
     </div>
   );
 }
